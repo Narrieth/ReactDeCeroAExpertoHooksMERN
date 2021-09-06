dab8523a8447d0439743368d868cf044a8ff8e69
Narrieth <narrieth14ajc@gmail.com>
2021-09-06 19:06:46 -0400

    impresion de variables en el html

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -8,11 +8,16 @@ const PrimeraApp = () => {
         nombre:'narri',
         edad:26
     };
-    const saludo1='Hola jeje'
+    const saludo1='Hola jeje';
+    const array=[1,2,3,4,5];
+    const enteros=1;
+
     return (
         <>
             <h1> {saludo1} </h1>
-            {/* <pre>{JSON.stringify(saludo,null,3)}</pre> */}
+            <pre>{JSON.stringify(saludo,null,3)}</pre>
+            <h2> {array} </h2>
+            <h3> {enteros} </h3>
             <p>aaaa</p>
         </>
     );
